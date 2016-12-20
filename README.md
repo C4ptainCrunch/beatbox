6c50ba397b1900a803faad7100d1a1dc72df50be
Nikita Marchant <nikita.marchant@gmail.com>
2016-12-20 13:19:43 +0100

    J'ai dit YOLO, je peux

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # beatbox
-Mais oui c'est clair
+
+Crossbar and React swag for a useless Beatbox in your kitchen.
+
+# Warning !
+At the moment, this app is **VULNERABLE TO SHELL INJECTION**. Do **NOT** run in untrusted environments.
+
+# Install
+
+    virtualenv ve
+    source ve/bin/activate
+    pip install -r requirements.txt
+    crossbar run & python app.py
+
+Fill `samples/` with mp3 files then go to `http://localhost:8080`` and have fun !
+
+# Screenshot
+
+...
---
+++ app.py
@@ -0,0 +1,39 @@
+
+import asyncio
+from os import environ
+import datetime
+import glob
+import base64
+import json
+
+from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
+
+
+class Component(ApplicationSession):
+    async def onJoin(self, details):
+        player_lock = asyncio.Lock()
+
+        def get_samples():
+            self.log.info("Samples list requested")
+            samples = [b[8:-4] for b in glob.glob("samples/*.mp3")]
+            return samples
+
+        await self.register(get_samples, u'nefliers.beatbox.samples.list')
+
+        async def play_samples(name):
+            with await player_lock:
+                ############################################
+                # THIS IS VULNERABLE TO SHELL INJECTIONS ! #
+                ############################################
+                await asyncio.create_subprocess_shell('mplayer -really-quiet "samples/%s.mp3" 2&1> /dev/null' % name)
+                self.log.info("Playing '{song}'", song=name)
+
+        await self.register(play_samples, u'nefliers.beatbox.samples.play')
+
+
+if __name__ == '__main__':
+    runner = ApplicationRunner(
+        environ.get("AUTOBAHN_DEMO_ROUTER", u"ws://127.0.0.1:8080/ws"),
+        u"realm1",
+    )
+runner.run(Component)
